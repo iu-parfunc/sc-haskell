228062ffc50c9a81677f1d0348d93a0046353f20
Ryan Newton <rrnewton@gmail.com>
2016-03-25 15:52:58 -0400

    Update script

--- SurveyScripts/hackage-mirror.sh
+++ SurveyScripts/hackage-mirror.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+outdir=./data/0_hackage_all_tarballs/
+
 echo "Cleaning up..."
 rm 00-index.tar.gz
-mkdir -p package
+mkdir -p $outdir
 echo "Downloading index..."
 
-if [ ! -e 00-index.tar.gz ] ; then 
+if [ ! -e 00-index.tar.gz ] ; then
   wget http://hackage.haskell.org/packages/archive/00-index.tar.gz
 fi
 
@@ -14,8 +16,9 @@ for splitpk in `tar tf 00-index.tar.gz | grep -v preferred | cut -d/ -f 1,2`; do
         echo Downloading $pk ...
 	name=$pk.tar.gz
 	if [ ! -e package/$name ]; then
-	   wget http://hackage.haskell.org/package/$pk/$name -O package/$name
+	   wget http://hackage.haskell.org/package/$pk/$name -O $outdir/$name
 	fi
 done
 
+# Example package URL:
 # https://hackage.haskell.org/package/lvish-1.1.4/lvish-1.1.4.tar.gz
