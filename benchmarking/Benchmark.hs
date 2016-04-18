5ae2318163b815d3579ab2f186bac8ef6cd9511a
RyanGlScott <ryan.gl.scott@gmail.com>
2016-04-17 20:50:55 -0400

    Actually download stuff from the interwebs

--- benchmarking/Benchmark.hs
+++ benchmarking/Benchmark.hs
@@ -1,6 +1,19 @@
+module Main (main) where
+
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as L
+import Data.Maybe (mapMaybe)
+import Data.Version (makeVersion)
+
 import Distribution.Compat.ReadP
-import Distribution.Package (Dependency)
+import Distribution.Package (Dependency(..), PackageIdentifier(..), PackageName(..))
+import Distribution.PackageDescription.Parse (ParseResult(..), parsePackageDescription)
 import Distribution.Text (Text(..), simpleParse)
+import Distribution.Version (isSpecificVersion)
+
+import Network.HTTP.Client
+
+import System.FilePath ((</>), (<.>))
 
 comments :: ReadP r ()
 comments = do
@@ -21,9 +34,29 @@ instance Text CabalConfig where
   disp = undefined
   parse = CabalConfig <$> parser
 
+toPackageIdentifier :: Dependency -> Maybe PackageIdentifier
+toPackageIdentifier (Dependency p vr) =
+  PackageIdentifier p <$> isSpecificVersion vr
+
+downloadCabalFile :: Manager -> PackageIdentifier -> IO ByteString
+downloadCabalFile m pkgId = do
+    let pkgIdStr = show $ disp pkgId
+        pkgStr   = show $ disp $ case pkgId of PackageIdentifier pkg _ -> pkg
+    initialRequest <- parseUrl $ "GET http://hackage.haskell.org/package"
+                              </> pkgIdStr </> pkgStr <.> "cabal"
+    responseBody <$> httpLbs initialRequest m
+
 main :: IO ()
 main = do
-    cnf <- readFile $! "cabal.config"
-    case simpleParse cnf :: Maybe CabalConfig of
-         Nothing -> error "wat"
-         Just cc -> print cc
+    {-
+    cnf <- readFile $! "test.config"
+    let vers = case simpleParse cnf :: Maybe CabalConfig of
+          Nothing -> error "Parse error"
+          Just (CabalConfig cc) -> mapMaybe toPackageIdentifier cc
+    -}
+    manager   <- newManager defaultManagerSettings
+    cabalFile <- downloadCabalFile manager (PackageIdentifier (PackageName "hello") (makeVersion [1,0,0,2]))
+    let pkgDescr = case parsePackageDescription (L.unpack cabalFile) of
+                     ParseFailed pe -> error $ show pe
+                     ParseOk _ d    -> d
+    print pkgDescr
