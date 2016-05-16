2b3f5aeb1630459612b2238ea1655c2cc4bdaa44
Ryan Scott <ryan.gl.scott@gmail.com>
2016-05-16 15:57:59 -0400

    Make sure to create results directory

--- benchmarking/benchmark-via-jenkins.sh
+++ benchmarking/benchmark-via-jenkins.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -xe
 
+if [ "${BUILD_TAG}" == "" ]; then
+  BUILD_TAG="jenkins-unknown"
+fi
+
+if [ "${NODE_NAME}" == "" ]; then
+  NODE_NAME="node-unknown"
+fi
+
 RESULTS_DIR=${HOME}/results_backup/benchmark_stackage/${BUILD_TAG}/${NODE_NAME}
 
-./benchmarking/Benchmark.hs
-cp -r .bench-build ${RESULTS_DIR}/
+./Benchmark.hs
+mkdir -p ${RESULTS_DIR}
+cp -r .bench-res ${RESULTS_DIR}/
