87cad6db1db483de618e66f2ae82f7ec1f7cdf5c
Ryan Scott <ryan.gl.scott@gmail.com>
2016-05-17 15:57:40 -0400

    Take two

--- benchmarking/benchmark-via-jenkins.sh
+++ benchmarking/benchmark-via-jenkins.sh
@@ -11,12 +11,12 @@ fi
 
 RESULTS_DIR=${HOME}/results_backup/benchmark_stackage/${BUILD_TAG}/${NODE_NAME}
 
-/opt/modules/Modules/3.2.10/init/bash
-module add llvm
+eval `/opt/modules/Modules/3.2.10/bin/modulecmd bash add llvm`
 C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu:/usr/include:$C_INCLUDE_PATH
 LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib:$LIBRARY_PATH
 LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH
 PKG_INCLUDE_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/pkgconfig:$PKG_CONFIG_PATH
+PATH=$PATH:/usr/bin
 
 ./Benchmark.hs
 mkdir -p ${RESULTS_DIR}
