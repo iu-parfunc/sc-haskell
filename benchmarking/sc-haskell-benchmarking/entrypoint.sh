cf0660daa91f8380d4ad35f9fd9056492a39545b
Ryan Scott <ryan.gl.scott@gmail.com>
2016-06-21 15:24:46 -0400

    Possibly my ugliest hack yet

--- benchmarking/sc-haskell-benchmarking/entrypoint.sh
+++ benchmarking/sc-haskell-benchmarking/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+RUN_THIS=""
+
+for ARGG in "$@"
+do
+    RUN_THIS="$RUN_THIS\"$ARGG\" "
+done
+
 ln -fs /root/lorem-ipsum.txt /dev/urandom
-faketime -f '2008-12-24 08:15:42' /bin/bash -c "$*"
+faketime -f '2008-12-24 08:15:42' /bin/bash -c "${RUN_THIS}"
