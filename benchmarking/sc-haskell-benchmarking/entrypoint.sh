c623f75a0245e40947aaf0947aa590f762aabf83
Ryan Scott <ryan.gl.scott@gmail.com>
2016-06-21 19:14:21 -0400

    Let's fake a more modern year

--- benchmarking/sc-haskell-benchmarking/entrypoint.sh
+++ benchmarking/sc-haskell-benchmarking/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-RUN_THIS=""
-
-for ARGG in "$@"
-do
-    RUN_THIS="$RUN_THIS\"$ARGG\" "
-done
-
 ln -fs /root/lorem-ipsum.txt /dev/urandom
-faketime -f '2008-12-24 08:15:42' /bin/bash -c "${RUN_THIS}"
+faketime -f '2016-12-24 08:15:42' "$@"
