9397f3ff36ef4298d7a6f4ab45e47abf91190b71
Ryan Newton <rrnewton@gmail.com>
2016-07-11 16:37:42 -0400

    WIP: add a from file as well as from-memory Handle test

--- microbenchmarks/Handle/Main.hs
+++ microbenchmarks/Handle/Main.hs
@@ -10,6 +10,8 @@ import Data.Knob
 import Data.ByteString.Char8 as B
 import System.IO as I
 
+import System.Posix.Temp (mkstemp)
+
 test01 :: Int -> IO ByteString
 test01 n = do
   knob <- newKnob (pack [])
@@ -19,7 +21,7 @@ test01 n = do
     B.hPutStrLn h "Hello world!"
 
   I.putStrLn "START GETS"
-
+  I.hSeek h AbsoluteSeek 0 
   forM_ [1..n] $ \_ -> do
     _l <- B.hGetLine h
     return ()
@@ -30,8 +32,30 @@ test01 n = do
   Data.Knob.getContents knob
 
 
+test02 :: Int -> IO ()
+test02 n = do
+  knob <- newKnob (pack [])
+  (f,h) <- mkstemp "test.txt" 
+  forM_ [1..n] $ \_ -> do
+    B.hPutStrLn h "hi"
+  hClose h
+
+  h2 <- openFile f ReadMode
+  I.putStrLn "START GETS"
+  forM_ [1..n] $ \_ -> do
+    _l <- B.hGetLine h2
+    return ()
+  hClose h2
+  I.putStrLn "DONE GETS"
+
+
+
 main :: IO ()
 main =
-  do x <- test01 100
+  do I.putStrLn "Test01"
+     x <- test01 100
      print x
+
+     I.putStrLn "Test02"
+     test02 100
 
