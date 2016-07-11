789983de3fdf4d994f6fffba645ddae527bf06f5
Ryan Newton <rrnewton@gmail.com>
2016-07-11 17:00:31 -0400

    Criterionize

--- microbenchmarks/Handle/Main.hs
+++ microbenchmarks/Handle/Main.hs
@@ -6,7 +6,8 @@ module Main where
 
 import Control.Monad
 import Data.Knob
--- import Criterion.Main
+import Criterion.Main
+import Criterion.Types
 import Data.ByteString.Char8 as B
 import System.IO as I
 
@@ -18,16 +19,11 @@ test01 n = do
   h <- newFileHandle knob "test.txt" ReadWriteMode
   -- B.hPutStrLn h "Start writing."
   forM_ [1..n] $ \_ -> do
-    B.hPutStrLn h "Hello world!"
-
-  I.putStrLn "START GETS"
+    B.hPutStrLn h "hi"
   I.hSeek h AbsoluteSeek 0 
   forM_ [1..n] $ \_ -> do
     _l <- B.hGetLine h
     return ()
-
-  I.putStrLn "DONE GETS"
-    
   hClose h
   Data.Knob.getContents knob
 
@@ -39,23 +35,15 @@ test02 n = do
   forM_ [1..n] $ \_ -> do
     B.hPutStrLn h "hi"
   hClose h
-
   h2 <- openFile f ReadMode
-  I.putStrLn "START GETS"
   forM_ [1..n] $ \_ -> do
     _l <- B.hGetLine h2
     return ()
   hClose h2
-  I.putStrLn "DONE GETS"
-
 
 
 main :: IO ()
-main =
-  do I.putStrLn "Test01"
-     x <- test01 100
-     print x
-
-     I.putStrLn "Test02"
-     test02 100
-
+main = defaultMain
+  [ bench "in-mem put-get lines"    $ Benchmarkable (void . test01 . fromIntegral)
+  , bench "from-disk put-get lines" $ Benchmarkable (       test02 . fromIntegral)
+  ]
