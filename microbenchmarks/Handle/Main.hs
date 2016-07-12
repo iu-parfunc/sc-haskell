eac41986d6adde3d99fda3ff9bba421e9d24bcd5
Parfunc Account <parfunc@googlegroups.com>
2016-07-12 15:07:22 -0400

    Add more microbenchmarks

--- microbenchmarks/Handle/Main.hs
+++ microbenchmarks/Handle/Main.hs
@@ -10,7 +10,7 @@ import Criterion.Main
 import Criterion.Types
 import Data.ByteString.Char8 as B
 import System.IO as I
-
+import System.Directory (removeFile)
 import System.Posix.Temp (mkstemp)
 
 test01 :: Int -> IO ByteString
@@ -28,22 +28,31 @@ test01 n = do
   Data.Knob.getContents knob
 
 
-test02 :: Int -> IO ()
-test02 n = do
-  knob <- newKnob (pack [])
-  (f,h) <- mkstemp "test.txt" 
+-- test02 :: FilePath -> Int -> IO ()
+test02 :: Handle -> Int -> IO ()
+test02 h n = do
+  -- h <- openFile f ReadMode
+  hSeek h AbsoluteSeek 0
   forM_ [1..n] $ \_ -> do
     B.hPutStrLn h "hi"
-  hClose h
-  h2 <- openFile f ReadMode
+  -- hClose h
+  -- h2 <- openFile f ReadMode
+  let h2 = h
+  hSeek h2 AbsoluteSeek 0
   forM_ [1..n] $ \_ -> do
     _l <- B.hGetLine h2
-    return ()
-  hClose h2
-
+    return ()    
+  -- hClose h2
 
 main :: IO ()
-main = defaultMain
-  [ bench "in-mem put-get lines"    $ Benchmarkable (void . test01 . fromIntegral)
-  , bench "from-disk put-get lines" $ Benchmarkable (       test02 . fromIntegral)
-  ]
+main = do
+  (f,h) <- mkstemp "/tmp/test.txt"
+  -- hClose h
+  defaultMain
+   [ bench "in-mem put-N get-N lines"    $ Benchmarkable (void . test01 . fromIntegral)
+   , bench "from-disk put-N get-N lines" $ Benchmarkable (       test02 h . fromIntegral)
+   , bench "in-mem put-1000 get-1000"    $ whnfIO $ void $ test01 1000
+   , bench "from-disk put-1000 get-1000" $ whnfIO $ test02 h 1000
+   ]
+  I.putStrLn $ "Removing temp file: "++ f
+  removeFile f
