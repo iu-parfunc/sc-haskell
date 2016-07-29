a95cdd5194f98e789aca021eaf00c3393631346e
Parfunc Account <parfunc@googlegroups.com>
2016-07-29 16:43:14 -0400

    check in my little test cript

--- microbenchmarks/Handle/run_dbg.sh
+++ microbenchmarks/Handle/run_dbg.sh
@@ -5,7 +5,8 @@ set -xe
 rm -f *.eventlog; 
 
 # IMG=parfunc/sc-haskell:v0.4-dbg
-IMG=parfunc/sc-haskell:v0.4-opt-dbg
+# IMG=parfunc/sc-haskell:v0.4-opt-dbg
+IMG=parfunc/sc-haskell:v0.5-dbg
 
 docker run -it $IMG ghc --version
 stack --docker --docker-image=$IMG bench --benchmark-arguments="--iters 1 from-disk/put-1000_get-1000"
